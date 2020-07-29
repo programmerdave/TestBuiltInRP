890fe17b771cc56818b8bfe806625ea66f755ae9
David Rodriguez <programmerdave@gmail.com>
2020-07-29 16:04:21 -0400

    Intial Commit

---
+++ Assets/Scripts/QuadUtility.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadUtility : MonoBehaviour
+{
+    public Camera mainCamera;
+    public LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void ResetQuadPosition()
+    {
+        float forwardDist = 3.0f; // position away from the camera
+        Vector3 camForward = mainCamera.transform.forward;
+        Vector3 camPos = mainCamera.transform.position;
+
+        Vector3 projectedForward = Vector3.ProjectOnPlane(camForward, Vector3.up).normalized * forwardDist;
+
+        Vector3 quadPos = projectedForward + camPos + new Vector3(0, -1.2f, 0);
+        float quadBottom = quadPos.y - this.gameObject.transform.localScale.y/2;
+
+        //make sure it doesn't go below the global floor
+        //This has to be adjusted to raycast to find the plane underneath instead of using global zero
+        if(quadBottom < 0)
+        {
+            quadPos.y = this.gameObject.transform.localScale.y/2;
+        }
+
+        lineRenderer.SetPositions(new Vector3[] { camPos, quadPos });
+
+        this.gameObject.transform.position = quadPos;
+        this.gameObject.transform.LookAt(camPos);
+        this.gameObject.transform.Rotate(this.gameObject.transform.up, 180.0f, Space.World);
+        //this.gameObject.transform.LookAt(camPos);
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject quadObj;
+    public QuadUtility quadUtility;
+    public GameObject lineRendererGameObject;
+
+    bool isMenuVisible = false;
+    public TextMeshProUGUI showMenuButtonText;
+    public GameObject hideMenuButton;
+   
+    private void Start()
+    {
+        UpdateButtons();
+    }
+
+    public void OnShowMenuButtonClick()
+    {
+        isMenuVisible = true;
+        UpdateButtons();
+    }
+
+    public void OnHideMenuButtonClick()
+    {
+        isMenuVisible = false;
+        UpdateButtons();
+    }
+
+    public void UpdateButtons()
+    {
+        hideMenuButton.SetActive(isMenuVisible);
+        lineRendererGameObject.SetActive(isMenuVisible);
+        quadObj.SetActive(isMenuVisible);
+        quadUtility.ResetQuadPosition();
+
+        if (isMenuVisible)
+        {
+            showMenuButtonText.text = "Reset Menu";
+        }
+        else
+        {
+            showMenuButtonText.text = "Show Menu";
+        }
+    }
+}
